053f584341cf96c3b84d80e2781c007178617f66
Mahmoud Abusalem <116794637+mahmoudabusalem@users.noreply.github.com>
2024-08-22 15:28:30 +0300

    [125] [Time Beats: 44.99%] [Memory Beats: 25.29%] - LeetPush

---
+++ 125-Valid-Palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string filter(const string& s) {
+        string t;
+        for (char c : s) {
+            if (isalpha(c) || isdigit(c)) {
+                t += tolower(c);
+            }
+        }
+        return t;
+    }
+    bool isPalindrome(string s) {
+        s = filter(s);
+        int i = 0, j = s.size() - 1;
+        while (i < j)
+        {
+            if (s[i++] != s[j--])
+                return false;
+        }
+        return true;
+    }
+};
