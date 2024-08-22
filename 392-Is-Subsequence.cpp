a9338023eb7ef3743de54176a2b7d049cde836b7
Mahmoud Abusalem <116794637+mahmoudabusalem@users.noreply.github.com>
2024-08-22 16:02:01 +0300

    [392] [Time Beats: 100.00%] [Memory Beats: 52.88%] - LeetPush

---
+++ 392-Is-Subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int idx_s = 0;
+        int idx_t = 0;
+
+        while (idx_s < s.length() && idx_t < t.length()) {
+            if (s[idx_s] == t[idx_t]) {
+                idx_s++;  
+            }
+            idx_t++;  
+        }
+
+        return idx_s == s.length();
+    }
+};
