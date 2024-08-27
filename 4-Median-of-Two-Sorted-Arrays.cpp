49133cd7b9ee6c59c0f41015683d6f41ee8816c9
Mahmoud Abusalem <116794637+mahmoudabusalem@users.noreply.github.com>
2024-08-27 23:12:45 +0300

    [4] [Time Beats: 99.37%] [Memory Beats: 78.54%] - LeetPush

--- 4-Median-of-Two-Sorted-Arrays.cpp
+++ 4-Median-of-Two-Sorted-Arrays.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        int n = a.size();
+        int m = b.size();
 
-        int size = (n + m); // size is odd -> mid
-                            // size is even -> (mid1 + mid2) / 2
-        
         if (n > m) {
-           nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-           sort(nums1.begin(), nums1.end());
-            if(size % 2){
-                return (double)nums1[size/2];
-            }
-            else{
-                int mid1 = nums1[size/2 - 1], mid2 = nums1[size/2];
-                return (double)(mid1 + mid2) / 2;
-            }
-        } else {
-            nums2.insert(nums2.end(), nums1.begin(), nums1.end());
-            sort(nums2.begin(), nums2.end());
-            if(size % 2){ // 3   [2]
-                int idx = size / 2;
-                return (double)nums2[size/2];
-            }
-            else{
-                int mid1 = nums2[size/2 - 1], mid2 = nums2[size/2];
-                return (double)(mid1 + mid2) / 2;
+            swap(a, b);
+            swap(n, m);
+        }
+
+        int l = 0, r = n;
+        while (l <= r) {
+            int p1 = (l + r) / 2;
+            int p2 = (n + m + 1) / 2 - p1;
+
+            int maxL1 = (p1 == 0) ? INT_MIN : a[p1 - 1];
+            int minR1 = (p1 == n) ? INT_MAX : a[p1];
+
+            int maxL2 = (p2 == 0) ? INT_MIN : b[p2 - 1];
+            int minR2 = (p2 == m) ? INT_MAX : b[p2];
+
+            if (maxL1 <= minR2 && maxL2 <= minR1) {
+                if ((n + m) % 2 == 0) {
+                    return (max(maxL1, maxL2) + min(minR1, minR2)) / 2.0;
+                } else {
+                    return max(maxL1, maxL2);
+                }
+            } else if (maxL1 > minR2) {
+                r = p1 - 1;
+            } else {
+                l = p1 + 1;
             }
         }
-        return 0.0;
+
+        return 0;
     }
 };
