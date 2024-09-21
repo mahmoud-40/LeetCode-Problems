f5bcbc1e7c810d98ebfcba0bcd0b446cac0c0aff
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2024-09-21 15:15:53 +0300

    [386] [Time Beats: 58.57%] [Memory Beats: 76.44%] - LeetPush

---
+++ 386-Lexicographical-Numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> result;
+        for (int i = 1; i <= 9; i++) {
+            dfs(i, n, result);
+        }
+        return result;
+    }
+
+private:
+    void dfs(int curr, int n, vector<int>& result) {
+        if (curr > n) return;
+        result.push_back(curr);
+        for (int i = 0; i <= 9; i++) {
+            dfs(curr * 10 + i, n, result);
+        }
+    }
+};
