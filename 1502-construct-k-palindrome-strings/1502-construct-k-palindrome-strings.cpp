1543d966ab968e516768c731ca6961d5beacefc4
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2025-01-11 05:05:36 +0200

    Time: 39 ms (5.59%), Space: 15.1 MB (21.68%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
-        map<char, int> mp;
-        // set<char> st;
+        // map<char, int> mp;
+        int mp[26];
+        set<char> st;
 
-        for(auto ch : s){
-            mp[ch]++;
-            // st.insert(ch);
+        for(auto ch : s){ 
+            mp[ch - 'a']++;
+            st.insert(ch);
         }
 
         int cnt = 0;
 
-        for(auto ch : s){
-            if((mp[ch] % 2 != 0)){
+        for(auto ch : st){ 
+            if((mp[ch - 'a'] % 2 != 0)){ 
                 cnt++;
-                mp[ch] = 0;
-            }
-
-            if(cnt > k){
-                return false;
             }
         }
 
