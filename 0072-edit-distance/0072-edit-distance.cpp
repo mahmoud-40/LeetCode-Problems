318b1fde18d0669cdf9a187ad224b6f1e9368fe9
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2025-04-09 14:20:34 +0200

    Time: 4 ms (80.26%), Space: 14.2 MB (21.33%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int minDistanceHelper(string& word1, string& word2, int i, int j, vector<vector<int>>& memo) {
-        if (i == word1.length()) {
-            // Insert all remaining characters of word2
-            return word2.length() - j;
+        if(i == word1.size()){
+            return word2.size() - j;
         }
-        if (j == word2.length()) {
-            // Delete all remaining characters of word1
-            return word1.length() - i;
+
+        if(j == word2.size()){
+            return word1.size() - i;
         }
-        
-        if (memo[i][j] != -1) {
+
+        if(memo[i][j] != -1){
             return memo[i][j];
         }
-        
-        if (word1[i] == word2[j]) {
-            memo[i][j] = minDistanceHelper(word1, word2, i + 1, j + 1, memo);
-            return memo[i][j];
+
+        if(word1[i] == word2[j]){
+            return memo[i][j] = minDistanceHelper(word1, word2, i + 1, j + 1, memo);
         }
-        
+
         int insertOp = minDistanceHelper(word1, word2, i, j + 1, memo);
         int deleteOp = minDistanceHelper(word1, word2, i + 1, j, memo);
         int replaceOp = minDistanceHelper(word1, word2, i + 1, j + 1, memo);
-        
+
         return memo[i][j] = 1 + min({insertOp, deleteOp, replaceOp});
     }
 
