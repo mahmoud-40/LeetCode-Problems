4f2a761de95dc6a5915d843bb74f63edd9db0abd
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2025-04-10 16:59:29 +0200

    Time: 7 ms (48.72%), Space: 15.7 MB (10.18%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(mp[target - nums[i]]){
+                return {i, mp[target - nums[i]] - 1};
+            }
+
+            mp[nums[i]] = i + 1;
+        }
+
+        return {};
+    }
+};
