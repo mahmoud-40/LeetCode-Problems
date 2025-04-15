f6821d0e28df48ac513f91e294e19a0151c2ddf7
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2025-04-15 02:33:26 +0200

    Time: 0 ms (100%), Space: 13.3 MB (76.46%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,45 +2,36 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();  
-        int n = matrix[0].size();  
+        int n = matrix[0].size(); 
+       
+        int l = 0;
+        int r = (m - 1);
 
-        int l = 0, r = (m * n) - 1;
-    
-        while (l <= r){
+        int row = -1; 
+
+        while (l <= r) {
             int mid = l + (r - l) / 2;
-            
-            int row = mid / n;
-            int col = mid % n;
 
-            int current = matrix[row][col];
-            
-            if (current == target) {
-                return true; 
-            } else if (current > target) {
-                r = mid - 1;; 
-            } else {
-                l = mid + 1; 
+            if (matrix[mid][0] <= target && target <= matrix[mid][n - 1]) {
+                row = mid;
+                break;
+            }
+            else if (matrix[mid][0] < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
             }
         }
 
-        return false; 
-    }
-};
+        if(row == -1) return false;
 
-/*
-- Time Complexity = m * log(n)
-- Space Complexity = o(1)
-
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i = 0; i < matrix.size(); i++){
-            int x = lower_bound(matrix[i].begin(), matrix[i].end(), target) - matrix[i].begin();
-            if(x < matrix[i].size() && matrix[i][x] == target){
-                return true;
-            }
+        int x = lower_bound(matrix[row].begin(), matrix[row].end(), target) - matrix[row].begin();
+        
+        if(x < matrix[row].size() && matrix[row][x] == target){
+            return true;
         }
+
         return false;
     }
 };
-*/
