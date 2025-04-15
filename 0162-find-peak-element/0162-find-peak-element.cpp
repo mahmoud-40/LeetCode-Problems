5da1a3066956f10497eb14d7426c5c7ef865e548
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2025-04-15 03:21:49 +0200

    Time: 0 ms (100%), Space: 12.6 MB (32.72%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,14 +4,8 @@ public:
         int l = 0, r = nums.size() - 1;
         while (l < r) {
             int mid = l + (r - l) / 2;
-
-            if ((mid == 0 || nums[mid] > nums[mid - 1]) && 
-                (mid == nums.size() - 1 || nums[mid] > nums[mid + 1])) {
-                return mid;
-            }
-            
-            if (mid > 0 && nums[mid] < nums[mid - 1]) {
-                r = mid - 1;
+            if (nums[mid] > nums[mid + 1]) {
+                r = mid;
             } else {
                 l = mid + 1;
             }
