7149da888d9c05fa276cc3e09be6ccb658d2a508
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2025-04-16 11:34:05 +0200

    Time: 0 ms (100%), Space: 8.1 MB (17.81%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    unordered_map<int, int> memo;
-
     int numTrees(int n) {
-        return getBST(n);    
+        vector<int> memo(n + 1, -1); 
+        return getBST(n, memo);    
     }
 
-    int getBST(int n){
+    int getBST(int n, vector<int> &memo){
         if(n == 0)
             return 1;
+            
         if(n == 1)
             return 1;
 
-        if(memo.count(n))
+        if (memo[n] != -1) 
             return memo[n];
 
         int left = 0, right = 0;
         int total = 0;
 
         for(int i = 1; i <= n; i++){
-            left = getBST(i - 1);
-            right = getBST(n - i);
+            left = getBST(i - 1, memo);
+            right = getBST(n - i, memo);
             total += left * right;
         }
 
