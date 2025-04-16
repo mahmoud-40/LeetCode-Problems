d1df95423bf2a4e34cfc2b8d1a47e29bf23609ba
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2025-04-16 18:21:48 +0200

    Time: 2037 ms (10.27%), Space: 7.8 MB (76.85%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int> uniqTree(n + 1, -1); 
-        return getBST(n, uniqTree);    
+        return getBST(n);    
     }
 
-    int getBST(int n, vector<int> &uniqTree){
+    int getBST(int n){
         if(n == 0)
             return 1;
 
         if(n == 1)
             return 1;
 
-        if (uniqTree[n] != -1) 
-            return uniqTree[n];
-
         int left = 0, right = 0;
         int total = 0;
 
         for(int i = 1; i <= n; i++){
-            left = getBST(i - 1, uniqTree);
-            right = getBST(n - i, uniqTree);
+            left = getBST(i - 1);
+            right = getBST(n - i);
             total += left * right;
         }
 
-        return uniqTree[n] = total;
+        return total;
     }
 };
