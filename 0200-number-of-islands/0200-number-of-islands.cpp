6aac1ac8cc7513e4a7e6ac56917ac2fc14636116
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2025-04-18 17:22:14 +0200

    Time: 29 ms (45.53%), Space: 16.3 MB (77.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-    void bfs(int i, int j, vector<vector<char>>& grid) {
-        // Directions for moving up, down, left, and right
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        grid[i][j] = '0'; // Mark as visited
+    void dfs(int i, int j, vector<vector<char>>& grid){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0')
+            return;
 
-        while (!q.empty()) {
-            auto [x, y] = q.front();
-            q.pop();
+        grid[i][j] = '0';
 
-            // Explore all four directions
-            for (auto& dir : directions) {
-                int newX = x + dir.first;
-                int newY = y + dir.second;
-
-                // Check boundaries and if the cell is land
-                if (newX >= 0 && newX < grid.size() && newY >= 0 && newY < grid[0].size() && grid[newX][newY] == '1') {
-                    grid[newX][newY] = '0'; // Mark as visited
-                    q.push({newX, newY}); // Add to the queue
-                }
-            }
-        }
+        dfs(i + 1, j, grid);
+        dfs(i - 1, j, grid);
+        dfs(i, j + 1, grid);
+        dfs(i, j - 1, grid);
     }
-
     int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty()) return 0;
-
         int lands = 0;
-        for (int i = 0; i < grid.size(); ++i) {
-            for (int j = 0; j < grid[0].size(); ++j) {
-                // If we find a '1', it's the start of a new island
-                if (grid[i][j] == '1') {
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[i].size(); j++){
+                if(grid[i][j] == '1'){
+                    dfs(i, j, grid);
                     lands++;
-                    bfs(i, j, grid); // perform BFS to mark the entire island
                 }
             }
         }
