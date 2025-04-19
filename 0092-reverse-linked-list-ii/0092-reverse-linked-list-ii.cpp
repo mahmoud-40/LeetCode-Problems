59bc3af7d34d0405323bb3c7e920afb6eb397105
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2025-04-19 23:08:08 +0200

    Time: 0 ms (100%), Space: 11.2 MB (72.78%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (!head || left == right) return head;
+        
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+        
+        // Move `prev` to the node before the `left` position
+        for (int i = 1; i < left; ++i) {
+            prev = prev->next;
+        }
+        
+        ListNode* curr = prev->next;
+        ListNode* next = nullptr;
+        
+        for (int i = left; i < right; ++i) {
+            next = curr->next;
+            curr->next = next->next;
+            next->next = prev->next;
+            prev->next = next;
+        }
+        
+        return dummy.next;
+    }
+};
