98e2b0b9b59144c69ffdfc6ba00674cc0bd28ae4
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2026-05-02 16:22:04 +0300

    [788] [Time Beats: 100.00%] [Memory Beats: 93.21%] - LeetPush

--- Rotated-Digits.java
+++ Rotated-Digits.java
@@ -1,38 +1,33 @@
 1class Solution {
 2    String s;
-3
-4    int dp(int[][][] memo, int pos, boolean tight, boolean hasChanged){
-5        if(pos == s.length()){
-6            return hasChanged ? 1 : 0;
-7        }
-8
-9        if(memo[pos][tight ? 1 : 0][hasChanged ? 1 : 0] != -1){
-10            return memo[pos][tight ? 1 : 0][hasChanged ? 1 : 0];
-11        }
-12
-13        int limit = tight ? (s.charAt(pos) - '0') : 9;
-14        int count = 0;
-15
-16        for (int d = 0; d <= limit; d++){
-17            if(d == 3 || d == 4 || d == 7) continue;
-18
-19            boolean newTight = tight && (d == limit);
-20            boolean newChanged = hasChanged || (d == 2 || d == 5 || d == 6 || d == 9);
-21
-22            count += dp(memo, pos + 1, newTight, newChanged);
-23        }
-24
-25        return memo[pos][tight ? 1 : 0][hasChanged ? 1 : 0] = count;
-26    }
-27
-28    public int rotatedDigits(int n) {
-29        s = String.valueOf(n);
-30        int[][][] memo = new int[s.length()][2][2];
-31
-32        for(int[][] a : memo)
-33            for(int[] b : a)
-34                Arrays.fill(b, -1);
-35
-36        return dp(memo, 0, true, false);
-37    }
-38}
+3    int[][] memo; 
+4
+5    int dp(int pos, boolean tight, boolean hasChanged){
+6        if(pos == s.length()) return hasChanged ? 1 : 0;
+7
+8        if(!tight && memo[pos][hasChanged ? 1 : 0] != -1)
+9            return memo[pos][hasChanged ? 1 : 0];
+10
+11        int limit = tight ? (s.charAt(pos) - '0') : 9;
+12        int count = 0;
+13
+14        for(int d = 0; d <= limit; d++){
+15            if(d == 3 || d == 4 || d == 7) continue;
+16            boolean newTight = tight && (d == limit);
+17            boolean newChanged = hasChanged || (d==2||d==5||d==6||d==9);
+18            count += dp(pos + 1, newTight, newChanged);
+19        }
+20
+21        if(!tight)
+22            memo[pos][hasChanged ? 1 : 0] = count;
+23
+24        return count;
+25    }
+26
+27    public int rotatedDigits(int n) {
+28        s = String.valueOf(n);
+29        memo = new int[s.length()][2];
+30        for(int[] row : memo) Arrays.fill(row, -1);
+31        return dp(0, true, false);
+32    }
+33}
