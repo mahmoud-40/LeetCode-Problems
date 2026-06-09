a39d0715af02b9825ac8cf48b08541fbd1c1681f
Mahmoud Abdulmawlaa <116794637+mahmoud-40@users.noreply.github.com>
2026-06-10 00:17:50 +0300

    [3689] [Time Beats: 100.00%] [Memory Beats: 52.21%] - LeetPush

--- Maximum-Total-Subarray-Value-I.java
+++ Maximum-Total-Subarray-Value-I.java
@@ -1,8 +1,13 @@
 1class Solution {
 2    public long maxTotalValue(int[] nums, int k) {
-3        long mx = Arrays.stream(nums).max().getAsInt();
-4        long mn = Arrays.stream(nums).min().getAsInt();
+3        long mx = Long.MIN_VALUE;
+4        long mn = Long.MAX_VALUE;
 5
-6        return (mx - mn) * k;
-7    }
-8}
+6        for(long num : nums){
+7            mx = Math.max(mx, num);
+8            mn = Math.min(mn, num);
+9        }
+10
+11        return (mx - mn) * k;
+12    }
+13}
